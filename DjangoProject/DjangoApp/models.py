e3abf379f2ea2ba714fce234747bf9a74cbb9a9d
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-05-10 16:39:09 +0700

    continue

--- DjangoProject/DjangoApp/models.py
+++ DjangoProject/DjangoApp/models.py
@@ -5,72 +5,92 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class UserType(models.TextChoices):
-    QUANTRI = 'QUANTRI'
-    CANHAN = 'CANHAN'
-    CUAHANG = 'CUAHANG'
+    NONE = 'NONE'
+    INDIVIDUAL = 'INDIVIDUAL'
+    SHOP = 'SHOP'
 
 class PurchaseType(models.TextChoices):
-    TRUCTUYEN = 'TRUCTUYEN'
-    TIENMAT = 'TIENMAT'
+    BANKING = 'BANKING'
+    CASH = 'CASH'
 
-class Buoi(models.TextChoices):
-    SANG = 'SANG'
-    CHIEU = 'CHIEU'
+class DaySession(models.TextChoices):
+    MORNING = 'MORNING'
+    AFTERNOON = 'AFTERNOON'
 
 class User(AbstractUser) :
     avatar = CloudinaryField('avatar',null=True)
-    ten = models.CharField(blank=False,null=False,max_length=200)
+    name = models.CharField(blank=False,null=False,max_length=200)
     email = models.TextField(blank=True,null=True)
-    sdt = models.CharField(blank=True,null=True,max_length=11)
-    type = models.CharField(max_length=7,choices=UserType.choices,default=UserType.CANHAN)
+    phone = models.CharField(blank=True,null=True,max_length=11)
+    type = models.CharField(max_length=10,choices=UserType.choices,default=UserType.INDIVIDUAL)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
     
 class Order(models.Model):
-    shop_id = models.ForeignKey('Shop',on_delete=models.CASCADE)
+    user = models.ForeignKey('User',on_delete=models.CASCADE,null=True,blank=False)
     is_valid = models.BooleanField(default=0)
-    loai_thanh_toan = models.CharField(max_length=10,choices=PurchaseType.choices,default=PurchaseType.TIENMAT)
-    ngay_order = models.DateField(auto_now_add=True)
+    payment_type = models.CharField(max_length=10,choices=PurchaseType.choices,default=PurchaseType.CASH)
+    date_order = models.DateField(auto_now_add=True)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
 
 class Dish(models.Model):
-    shop_id = models.ForeignKey('Shop',on_delete=models.CASCADE)
-    ten = models.CharField(blank=False,null=False,max_length=200)
-    tien_thuc_an = models.FloatField(blank=False,null=False,default=0)
+    shop = models.ForeignKey('Shop',on_delete=models.CASCADE,to_field='user_id',null=True,blank=False)
+    name = models.CharField(blank=False,null=False,max_length=200)
+    price = models.FloatField(blank=False,null=False,default=0)
     is_available = models.BooleanField(default=True,blank=True,null=True)
-    buoi = models.CharField(max_length=7,choices=Buoi.choices,blank=True,null=True)
-    chu_thich = models.TextField(blank=True,null=True)
+    day_session = models.CharField(max_length=10,choices=DaySession.choices,blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
 
 class Order_Dish(models.Model):
-    order_id = models.ForeignKey('Order', on_delete=models.CASCADE)
-    dish_id = models.ForeignKey('Dish',on_delete=models.CASCADE)
-    so_luong = models.IntegerField(default=1,blank=False,null=False)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    dish = models.ForeignKey('Dish',on_delete=models.CASCADE)
+    count = models.IntegerField(default=0,blank=False,null=False)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
 
     class Meta:
-        unique_together = ('order_id','dish_id')
+        unique_together = ('order','dish')
 
 class Shop(models.Model):
-    user = models.OneToOneField('user',on_delete=models.CASCADE,default=1)
-    dia_diem = models.TextField(blank=True,null=False)
+    user = models.OneToOneField('user',on_delete=models.CASCADE,default=1,primary_key=True)
+    location = models.TextField(blank=True,null=False)
     is_valid = models.BooleanField(default=0,blank=False,null=False)
-    tien_van_chuyen = models.FloatField(default=0,blank=False,null=False)
+    ship_payment = models.FloatField(default=0,blank=False,null=False)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
 
 class Menu(models.Model):
-    shop_id = models.ForeignKey('Shop',on_delete=models.CASCADE)
+    name = models.CharField(blank=False,null=False,default='Thực Đơn',max_length=350)
+    shop = models.ForeignKey('Shop',on_delete=models.CASCADE,to_field='user',null=True,blank=False)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
 
 class Menu_Dish(models.Model):
-    menu_id = models.ForeignKey('Menu', on_delete=models.CASCADE)
-    dish_id = models.ForeignKey('Dish',on_delete=models.CASCADE)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE, related_name='menu_dish_pairs')
+    dish = models.ForeignKey('Dish',on_delete=models.CASCADE)
+    count = models.IntegerField(default=1,blank=False,null=False)
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
     class Meta:
-        unique_together = ('menu_id','dish_id')
+        unique_together = ('menu','dish')
+
+class Comment(models.Model):
+    user = models.ForeignKey('User',on_delete=models.CASCADE,null=True,blank=False)
+    shop = models.ForeignKey('Shop',on_delete=models.CASCADE,null=True,blank=False,to_field='user')
+    dish = models.ForeignKey('Dish',on_delete=models.CASCADE,null=True,blank=False)
+    parent = models.ForeignKey('Comment',on_delete=models.CASCADE,null=True,blank=False)
+    content = models.TextField(blank=True,null=True)
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+
+class Rate(models.Model):
+    user = models.ForeignKey('User',on_delete=models.CASCADE,null=True,blank=False)
+    dish = models.ForeignKey('Dish',on_delete=models.CASCADE,null=True,blank=False)
+    stars = models.IntegerField(default=1,blank=False,null=False)
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    class Meta:
+        unique_together = ('user','dish')
--- DjangoProject/DjangoApp/views.py
+++ DjangoProject/DjangoApp/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets,permissions,parsers,status
 from rest_framework.viewsets import generics
 from rest_framework.response import Response
-from DjangoApp.serializers import UserSerializer
-from .models import Shop, User,UserType
+from .serializers import *
+from .pagination import DishPaginator
+from .models import *
 from rest_framework.decorators import action
-
+from .perms import ShopPermissions, SuperUserPermissions
 
 class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
     queryset = User.objects.all()
@@ -14,6 +15,8 @@ class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
     def get_permissions(self):
         if self.action.__eq__('current_user'):
             return [permissions.IsAuthenticated()]
+        if self.action.__eq__('verify_shop'):
+            return [SuperUserPermissions()]
         return [permissions.AllowAny()]
 
     @action(methods=['get'],detail=False,url_path='current-user')
@@ -23,21 +26,192 @@ class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
     
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        user=User.objects.get(pk=int(response.data['id']))
 
-        if user.type.__eq__(UserType.CUAHANG.__str__()):
+        if self.request.user.type.__eq__(UserType.CUAHANG.__str__()):
             shop = Shop()
-            shop.user = user
+            shop.user_id = self.request.user
             shop.dia_diem = request.data['dia_diem']
             shop.tien_van_chuyen = float(request.data['tien_van_chuyen'])
             shop.save()
 
         return response
     
+    @action(methods=['patch'],detail=True,url_path='verify-shop')
+    def verify_shop(selft, request, pk):
+        shop = Shop.objects.get(user_id=pk)
+        shop.is_valid = True
+        shop.save()
+        user = User.objects.get(pk=pk)
 
+        return Response(data={**UserSerializer(user).data,**ShopSerializer(shop).data},status=status.HTTP_200_OK)
 
+class DishViewSet(viewsets.ViewSet,generics.CreateAPIView,generics.ListAPIView):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+    permission_classes = [ShopPermissions]
+    pagination_class = DishPaginator
 
+    def get_permissions(self):
+        if self.action.__eq__('list'):
+            return [permissions.IsAuthenticated()]
+
+        return super().get_permissions()
+
+    def get_queryset(self):
+        shop_id = self.request.GET.get('shop_id')
+        name = self.request.GET.get('name')
+        from_price = self.request.GET.get('from_price')
+        to_price = self.request.GET.get('to_price')
+        is_available = self.request.GET.get('is_available')
+        day_session = self.request.GET.get('day_session')
+        queries = self.queryset
+
+        if shop_id:
+            queries = queries.filter(shop_id=shop_id)
+        if name:
+            queries = queries.filter(name__icontains=name)
+        if from_price or to_price:
+            queries = queries.filter(price__gt=float(from_price), price__lt=float(to_price))
+        if is_available:
+            queries = queries.filter(is_available=bool(is_available))
+        if day_session:
+            queries = queries.filter(day_session__icontains=day_session)
+        print(queries)
+        return queries
+
+class MenuViewSet(viewsets.ViewSet,generics.CreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [ShopPermissions]
+
+    def add_dishs_core(self, request, menu):
+        # request.data has a key 'dish'
+        # 'dish': [
+        #    {"id": 1, "count": 2},
+        #    {"id": 2, "count": 2},
+        #  ]
+        dish_list = request.data['dish']
+        for jobj in dish_list:
+            menu_dish = Menu_Dish()
+            menu_dish.menu = menu
+            menu_dish.dish = Dish.objects.get(pk=jobj['id'])
+            menu_dish.count = jobj['count']
+            menu_dish.save()
+
+    def create(self, request, *args, **kwargs):
+        menu_response = super().create(request, *args, **kwargs)
+        menu = Menu.objects.get(pk=int(menu_response.data['id']))
+        shop = Shop.objects.filter(user_id=self.request.user.id).first()
+        if shop:
+            menu.shop = shop
+        menu.save()
+        
+        self.add_dishs_core(request, menu)
+
+        return Response(data=MenuSerializer(menu).data,status=status.HTTP_201_CREATED)
     
-    
-    
+    @action(methods=['post'],detail=True,url_path='dishs')
+    def add_dish(self, request, pk):
+        menu = Menu.objects.get(pk=pk)
+        self.add_dishs_core(request, menu)
 
+        return Response(data=MenuSerializer(menu).data,status=status.HTTP_201_CREATED)
+    
+class OrderViewSet(viewsets.ViewSet, generics.CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action.__eq__('verify'):
+            return [ShopPermissions()]
+        return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        # request.data will have both keys: dish or menu
+        # {
+        #   payment_type: 'CASH'
+        #   dish: [
+        #    {'id':1,'count':2},
+        #    {'id':2,'count':3},
+        #   ],
+        #   menu: [1,2,3,4,5,...]
+        #}
+
+        response = super().create(request, *args, **kwargs)
+        user = self.request.user
+        order = Order.objects.get(pk=int(response.data['id']))
+        order.user = user
+        order.payment_type = response.data['payment_type']
+        order.save();
+
+        dish_list = request.data['dish']
+        menu_id_list = request.data['menu']
+
+        if dish_list or menu_id_list:
+        #   dish: [
+        #    {'id':1,'count':2},
+        #    {'id':2,'count':3},
+        #   ],
+            if dish_list:
+                for jobj in dish_list:
+                    order_dish = Order_Dish()
+                    dish = Dish.objects.get(pk=jobj['id'])
+                    count = jobj['count']
+                    order_dish.order = order
+                    order_dish.dish = dish
+                    order_dish.count += count
+                    order_dish.save()
+            
+        #   menu: [1,2,3,4,5,...]
+            if menu_id_list:
+                for id in menu_id_list:
+                    menu = Menu.objects.get(pk=id)
+                    md_pairs = menu.menu_dish_pairs.all()
+                    for pair in md_pairs:
+                        order_found = Order_Dish.objects.filter(order=order,dish=pair.dish).first()
+                        print(order_found)
+                        if (order_found):
+                            order_found.count += pair.count
+                            order_found.save()
+                        else:
+                            order_dish = Order_Dish()
+                            order_dish.order = order
+                            order_dish.dish = pair.dish
+                            order_dish.count += pair.count 
+                            order_dish.save()
+            
+        return Response(data=OrderSerializer(order).data,status=status.HTTP_201_CREATED)
+
+    @action(methods=['patch'],detail=True,url_path='verify')
+    def verify(selft, request, pk):
+        order = Order.objects.get(pk=pk)
+        order.is_valid = True
+        order.save()
+
+        return Response(data=OrderSerializer(order).data,status=status.HTTP_200_OK)
+
+class CommentViewSet(viewsets.ViewSet,generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        comment = Comment.objects.get(pk=int(response.data['id']))
+        print(self.request.user)
+        comment.user_id = self.request.user
+        comment.save()
+        return Response(data=CommentSerializer(comment).data,status=status.HTTP_201_CREATED)
+    
+class RateViewSet(viewsets.ViewSet,generics.CreateAPIView):
+    queryset = Rate.objects.all()
+    serializer_class = RateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        rate = Rate.objects.get(pk=int(response.data['id']))
+        rate.user_id = self.request.user
+        rate.save()
+        return Response(data=RateSerializer(rate).data,status=status.HTTP_201_CREATED)
