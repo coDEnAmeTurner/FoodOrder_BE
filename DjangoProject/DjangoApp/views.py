83682a6a20d4befee9d0c715944cc5463489fca1
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-05-13 23:48:35 +0700

    finished the business, left out stats

--- DjangoProject/DjangoApp/views.py
+++ DjangoProject/DjangoApp/views.py
@@ -45,7 +45,7 @@ class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
 
         return Response(data={**UserSerializer(user).data,**ShopSerializer(shop).data},status=status.HTTP_200_OK)
 
-class DishViewSet(viewsets.ViewSet,generics.CreateAPIView,generics.ListAPIView):
+class DishViewSet(viewsets.ViewSet,generics.UpdateAPIView, generics.CreateAPIView,generics.ListAPIView):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
     permission_classes = [ShopPermissions]
@@ -76,10 +76,9 @@ class DishViewSet(viewsets.ViewSet,generics.CreateAPIView,generics.ListAPIView):
             queries = queries.filter(is_available=bool(is_available))
         if day_session:
             queries = queries.filter(day_session__icontains=day_session)
-        print(queries)
         return queries
 
-class MenuViewSet(viewsets.ViewSet,generics.CreateAPIView):
+class MenuViewSet(viewsets.ViewSet,generics.UpdateAPIView, generics.CreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
     permission_classes = [ShopPermissions]
@@ -99,25 +98,46 @@ class MenuViewSet(viewsets.ViewSet,generics.CreateAPIView):
             menu_dish.save()
 
     def create(self, request, *args, **kwargs):
-        menu_response = super().create(request, *args, **kwargs)
-        menu = Menu.objects.get(pk=int(menu_response.data['id']))
-        shop = Shop.objects.filter(user_id=self.request.user.id).first()
-        if shop:
-            menu.shop = shop
-        menu.save()
-        
-        self.add_dishs_core(request, menu)
+        try:
+            menu_response = super().create(request, *args, **kwargs)
+            menu = Menu.objects.get(pk=int(menu_response.data['id']))
+            shop = Shop.objects.filter(user_id=self.request.user.id).first()
+            if shop:
+                menu.shop = shop
+            menu.save()
+            
+            self.add_dishs_core(request, menu)
 
-        return Response(data=MenuSerializer(menu).data,status=status.HTTP_201_CREATED)
+            return Response(data=MenuSerializer(menu).data,status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response(data={"error_msg":f"{str(e)}", "param_id":kwargs.get("pk")},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+    def update(self, request, *args, **kwargs):
+        try:
+            response = super().update(request, *args, **kwargs)
+            menu_id = response.data['id']
+            menu = Menu.objects.get(pk=int(menu_id))
+            Menu_Dish.objects.delete(menu_id=int(menu.id))
+            self.add_dishs_core(request, menu)
+            return response
+        except Menu.DoesNotExist:
+            return Response(data={"error_msg":f"Menu {kwargs.get("pk")} not found."},status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response(data={"error_msg":f"{e.__str__()}","param_id":kwargs.get("pk")},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     @action(methods=['post'],detail=True,url_path='dishs')
     def add_dish(self, request, pk):
-        menu = Menu.objects.get(pk=pk)
-        self.add_dishs_core(request, menu)
+        try:
+            menu = Menu.objects.get(pk=pk)
+            self.add_dishs_core(request, menu)
 
-        return Response(data=MenuSerializer(menu).data,status=status.HTTP_201_CREATED)
+            return Response(data=MenuSerializer(menu).data,status=status.HTTP_201_CREATED)
+        except Menu.DoesNotExist:
+            return Response(data={"error_msg":"Menu not found."},status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response(data={"error_msg":f"{str(e)}","param_id":pk},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-class OrderViewSet(viewsets.ViewSet, generics.CreateAPIView):
+class OrderViewSet(viewsets.ViewSet,generics.UpdateAPIView,  generics.CreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -138,80 +158,108 @@ class OrderViewSet(viewsets.ViewSet, generics.CreateAPIView):
         #   menu: [1,2,3,4,5,...]
         #}
 
-        response = super().create(request, *args, **kwargs)
-        user = self.request.user
-        order = Order.objects.get(pk=int(response.data['id']))
-        order.user = user
-        order.payment_type = response.data['payment_type']
-        order.save();
+        try:
+            response = super().create(request, *args, **kwargs)
+            user = self.request.user
+            order = Order.objects.get(pk=int(response.data['id']))
+            order.user = user
+            order.payment_type = response.data['payment_type']
+            order.save();
 
-        dish_list = request.data['dish']
-        menu_id_list = request.data['menu']
+            dish_list = request.data['dish']
+            menu_id_list = request.data['menu']
 
-        if dish_list or menu_id_list:
-        #   dish: [
-        #    {'id':1,'count':2},
-        #    {'id':2,'count':3},
-        #   ],
-            if dish_list:
-                for jobj in dish_list:
-                    order_dish = Order_Dish()
-                    dish = Dish.objects.get(pk=jobj['id'])
-                    count = jobj['count']
-                    order_dish.order = order
-                    order_dish.dish = dish
-                    order_dish.count += count
-                    order_dish.save()
-            
-        #   menu: [1,2,3,4,5,...]
-            if menu_id_list:
-                for id in menu_id_list:
-                    menu = Menu.objects.get(pk=id)
-                    md_pairs = menu.menu_dish_pairs.all()
-                    for pair in md_pairs:
-                        order_found = Order_Dish.objects.filter(order=order,dish=pair.dish).first()
-                        print(order_found)
-                        if (order_found):
-                            order_found.count += pair.count
-                            order_found.save()
-                        else:
+            if dish_list or menu_id_list:
+            #   dish: [
+            #    {'id':1,'count':2},
+            #    {'id':2,'count':3},
+            #   ],
+                if dish_list:
+                    for jobj in dish_list:
+                        try:
                             order_dish = Order_Dish()
+                            dish = Dish.objects.get(pk=jobj['id'])
+                            count = jobj['count']
                             order_dish.order = order
-                            order_dish.dish = pair.dish
-                            order_dish.count += pair.count 
+                            order_dish.dish = dish
+                            order_dish.count += count
                             order_dish.save()
-            
-        return Response(data=OrderSerializer(order).data,status=status.HTTP_201_CREATED)
-
+                        except Dish.DoesNotExist as od:
+                            return Response(data={"error_msg":f"Dish {jobj['id']} not found."},status=status.HTTP_404_NOT_FOUND)
+                        except Exception as od:
+                            return Response(data={"error_msg":f"{str(e)}"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                
+            #   menu: [1,2,3,4,5,...]
+                if menu_id_list:
+                    for id in menu_id_list:
+                        try:
+                            menu = Menu.objects.get(pk=id)
+                            md_pairs = menu.menu_dish_pairs.all()
+                            for pair in md_pairs:
+                                order_found = Order_Dish.objects.filter(order=order,dish=pair.dish).first()
+                                if (order_found):
+                                    order_found.count += pair.count
+                                    order_found.save()
+                                else:
+                                    order_dish = Order_Dish()
+                                    order_dish.order = order
+                                    order_dish.dish = pair.dish
+                                    order_dish.count += pair.count 
+                                    order_dish.save()
+                        except Menu.DoesNotExist as od:
+                            return Response(data={"error_msg":f"Menu {id} not found."},status=status.HTTP_404_NOT_FOUND)
+                        except Exception as od:
+                            return Response(data={"error_msg":f"{str(e)}"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(data=OrderSerializer(order).data,status=status.HTTP_201_CREATED)
+        except Order.DoesNotExist as od:
+            return Response(data={"error_msg":f"Order {response.data['id']} not found."},status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response(data={"error_msg":f"{str(e)}","param_id":kwargs.get("pk")},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
     @action(methods=['patch'],detail=True,url_path='verify')
     def verify(selft, request, pk):
-        order = Order.objects.get(pk=pk)
-        order.is_valid = True
-        order.save()
+        try:
+            order = Order.objects.get(pk=pk)
+            order.is_valid = True
+            order.save()
 
-        return Response(data=OrderSerializer(order).data,status=status.HTTP_200_OK)
+            return Response(data=OrderSerializer(order).data,status=status.HTTP_200_OK)
+        except Order.DoesNotExist as od:
+            return Response(data={"error_msg":f"Order {pk} not found."},status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response(data={"error_msg":f"{str(e)}","param_id":pk},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class CommentViewSet(viewsets.ViewSet,generics.CreateAPIView):
+class CommentViewSet(viewsets.ViewSet,generics.UpdateAPIView, generics.CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        comment = Comment.objects.get(pk=int(response.data['id']))
-        print(self.request.user)
-        comment.user_id = self.request.user
-        comment.save()
-        return Response(data=CommentSerializer(comment).data,status=status.HTTP_201_CREATED)
+        try:
+            response = super().create(request, *args, **kwargs)
+            comment = Comment.objects.get(pk=int(response.data['id']))
+            comment.user_id = self.request.user
+            comment.save()
+            return Response(data=CommentSerializer(comment).data,status=status.HTTP_201_CREATED)
+        except Comment.DoesNotExist as od:
+            return Response(data={"error_msg":f"Comment {response.data['id']} not found."},status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response(data={"error_msg":f"{str(e)}","param_id":kwargs.get("pk")},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-class RateViewSet(viewsets.ViewSet,generics.CreateAPIView):
+class RateViewSet(viewsets.ViewSet,generics.UpdateAPIView,generics.CreateAPIView):
     queryset = Rate.objects.all()
     serializer_class = RateSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        rate = Rate.objects.get(pk=int(response.data['id']))
-        rate.user_id = self.request.user
-        rate.save()
-        return Response(data=RateSerializer(rate).data,status=status.HTTP_201_CREATED)
+        try:
+            response = super().create(request, *args, **kwargs)
+            rate = Rate.objects.get(pk=int(response.data['id']))
+            rate.user_id = self.request.user
+            rate.save()
+            return Response(data=RateSerializer(rate).data,status=status.HTTP_201_CREATED)
+        except Rate.DoesNotExist as od:
+            return Response(data={"error_msg":f"Rate {response.data['id']} not found."},status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response(data={"error_msg":f"{str(e)}","param_id":kwargs.get("pk")},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
